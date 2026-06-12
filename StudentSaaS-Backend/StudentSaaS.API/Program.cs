66589c86c8efea6b7f1e0b5ad41a074fff6cb553
devdes1727-ux <devdes1727@gmail.com>
2026-06-12 11:03:37 +0530

    fix docker deploy

--- StudentSaaS-Backend/StudentSaaS.API/Program.cs
+++ StudentSaaS-Backend/StudentSaaS.API/Program.cs
@@ -70,5 +70,6 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://0.0.0.0:{port}");
 app.Run();
